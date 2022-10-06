a79ba27b29f54f45473923343f3c1bd1c06c49bb
Jonathan Santos <yurijonathan94@gmail.com>
2022-10-06 17:58:23 -0300

    Listando todas as categorias com o Data Transfer Object (DTO)

--- src/main/java/com/u/web/cursomc/services/CategoriaService.java
+++ src/main/java/com/u/web/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.u.web.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 
 }
